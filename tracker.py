d6287a95559c7dbde7fd3e87678da55bb50467ad
Frank Casanova <frankcasanova.info@gmail.com>
2024-05-08 21:07:28 +0200

    improved performance passing session client to machines

--- tracker.py
+++ tracker.py
@@ -1,9 +1,10 @@
-import httpx
+
 import asyncio  
 from selectolax.lexbor import LexborHTMLParser as HTMLParser
 import bisect
+from curl_cffi.requests import AsyncSession
 from setting import MMR_HEADERS, DEFAULT_HEADERS
-
+from asyncio import WindowsSelectorEventLoopPolicy
 #TODO:
 #URGENT, TRACKER HAS AN API, WE NEED THE WAY TO TAKE THE DATA FROM THE API
 #1. install fastapi
@@ -11,30 +12,42 @@ from setting import MMR_HEADERS, DEFAULT_HEADERS
 #3. create an endpoint for the tracker
 #4. create the frontend
 
-proxy = "http://41.89.16.6:80"
+asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
 
-async def get_html(url: str, headers: dict) -> HTMLParser:
-    """
-    Returns a HTMLParser object for the HTML content of the specified URL.
+# async def get_html(url: str, headers: dict) -> HTMLParser:
+#     """
+#     Returns a HTMLParser object for the HTML content of the specified URL.
 
-    Args:
-        url (str): The URL to get the HTML from.
+#     Args:
+#         url (str): The URL to get the HTML from.
+#         headers (dict): Headers to be included in the request.
 
-    Returns:
-        HTMLParser: A HTMLParser object for the HTML content of the specified URL.
-    """
-    async with httpx.AsyncClient(proxies={"http://": proxy}, verify=False, timeout=None) as client:
-        print(f'stablishing connection... to {url} using proxy {proxy}')
-        response: httpx.Response = await client.get(url, headers=headers)
-        print(f'connection established, status: {response.status_code}')
-        return HTMLParser(response.text)
+#     Returns:
+#         HTMLParser: A HTMLParser object for the HTML content of the specified URL.
+#     """
+#     try:
+#         async with AsyncSession(impersonate='edge101') as client:
+#             print(f'Establishing connection to {url}...')
+#             response = await client.get(url, headers=headers)
+#             print(f'Connection established, status: {response.status_code}')
+            
+#             # Validate response status code
+#             if response.status_code == 200:
+#                 return HTMLParser(response.text)
+#             else:
+#                 # Handle non-200 status codes
+#                 raise Exception(f'Request to {url} failed with status code: {response.status_code}')
+#     except Exception as e:
+#         # Handle exceptions gracefully
+#         print(f'An error occurred: {e}')
+#         return None  # Or raise an exception based on your error handling strategy
     
-async def get_json_data(url, headers: dict) -> dict:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-        response.raise_for_status()  # Raises an exception if the request was unsuccessful
-        json_data = response.json()
-        return json_data
+# async def get_json_data(url, headers: dict) -> dict:
+#     async with AsyncSession(impersonate='edge101') as client:
+#         response = await client.get(url, headers=headers)
+#         response.raise_for_status()  # Raises an exception if the request was unsuccessful
+#         json_data = response.json()
+#         return json_data
 
 def get_rank(mmr):
     thresholds = [125, 250, 375, 500, 625, 750, 875, 1000, 1125, 1250, 1375, 1500, 1625, 1750, 1875, 2000, 2125, 2250, 2375, 2500, 2625, 2750, 2875, 3000, 3125, 3250, 3375, 3500, 4000, 5000]
@@ -43,10 +56,12 @@ def get_rank(mmr):
     index = bisect.bisect_right(thresholds, mmr)
     return ranks[index] if index < len(ranks) else 'challenger'
 
-async def mmr(name: str):
+async def mmr(name: str, session: AsyncSession) -> dict:
     try:
         url = f"https://api.mylolmmr.com/api/mmr/euw1/{name}/420".replace('#', '%40').replace(' ', '%20')
-        json_data = await get_json_data(url, MMR_HEADERS)
+        # json_data = await get_json_data(url, MMR_HEADERS)
+        response = await session.get(url, headers=MMR_HEADERS)
+        json_data = response.json()
         mmr = json_data["mmr"]
         rank = get_rank(mmr)
         return {
@@ -59,7 +74,7 @@ async def mmr(name: str):
         }
 
 
-async def champ_info(name: str) -> dict[str, str]:
+async def champ_info(name: str, session: AsyncSession) -> dict[str, str]:
     """
     Returns a dictionary containing the result of getting the champion information from the specified URL.
 
@@ -72,7 +87,9 @@ async def champ_info(name: str) -> dict[str, str]:
     
         
     url = f'https://tracker.gg/lol/profile/riot/EUW/{name}/overview?playlist=RANKED_SOLO_5x5'.replace('#', '%23').replace(' ', '%20')
-    html = await get_html(url, DEFAULT_HEADERS)
+    # html = await get_html(url, DEFAULT_HEADERS)
+    response = await session.get(url, headers=DEFAULT_HEADERS)
+    html = HTMLParser(response.text)
     
     while not html.css_first('span.fit-text-parent > span'):
         await asyncio.sleep(5)
@@ -104,7 +121,7 @@ async def champ_info(name: str) -> dict[str, str]:
         
 
     
-async def wiki_info(top_1_used_champ: str) -> dict[str, str]:
+async def wiki_info(top_1_used_champ: str, session: AsyncSession) -> dict[str, str]:
     """
     Returns a dictionary containing the result of getting the wiki information from the specified URL.
 
@@ -116,7 +133,9 @@ async def wiki_info(top_1_used_champ: str) -> dict[str, str]:
     """
     try:
         url = f'https://leagueoflegends.fandom.com/wiki/{top_1_used_champ}/LoL#Hide_'
-        html = await get_html(url, DEFAULT_HEADERS)
+        # html = await get_html(url, DEFAULT_HEADERS)
+        response = await session.get(url, headers=DEFAULT_HEADERS)
+        html = HTMLParser(response.text)
         
         lore = html.css_first('div.skinviewer-info-lore > div:nth-child(1)').text()
         
@@ -128,7 +147,7 @@ async def wiki_info(top_1_used_champ: str) -> dict[str, str]:
 
     
 
-async def ingsingfull_info(top_1_used_champ: str) -> dict[str, str]:
+async def ingsingfull_info(top_1_used_champ: str, session: AsyncSession) -> dict[str, str]:
     """
     Returns a dictionary containing the result of getting the brief summary from the specified URL.
 
@@ -140,7 +159,10 @@ async def ingsingfull_info(top_1_used_champ: str) -> dict[str, str]:
     """
     try:
         url = f'https://lolalytics.com/lol/{top_1_used_champ}/build/'.lower()
-        html = await get_html(url, DEFAULT_HEADERS)
+        # html = await get_html(url, DEFAULT_HEADERS)
+        response = await session.get(url, headers=DEFAULT_HEADERS)
+        html = HTMLParser(response.text)
+        
         brief_summary = html.css_first('div.flex-1 > p').text()
         
         return {
@@ -151,39 +173,30 @@ async def ingsingfull_info(top_1_used_champ: str) -> dict[str, str]:
 
 
 async def main(name: str) -> dict[str, dict]:
-    """
-    Returns a dictionary containing the results of the following async functions:
-    - champ_info
-    - mmr
-    - wiki_info
-    - ingsingfull_info
+    try:
+        async with AsyncSession(impersonate='edge101') as session:
+            champ_info_task = asyncio.create_task(champ_info(name, session))
+            champ_info_result = await champ_info_task
+            top_1_used_champ = champ_info_result.get('top_1_used_champ', '')
 
-    Each value in the dictionary is a dictionary itself, representing the return value of the corresponding async function.
+            wiki_info_task = asyncio.create_task(wiki_info(top_1_used_champ, session))
+            ingsingfull_info_task = asyncio.create_task(ingsingfull_info(top_1_used_champ, session))
+            mmr_task = asyncio.create_task(mmr(name, session))
 
-    The return type of this function is `dict[str, dict]`. The keys of the outer dictionary are
-    the names of the async functions, and the values are the return values of each function.
-    """
-    
-    champ_info_task = asyncio.create_task(champ_info(name))
-    champ_info_result = await champ_info_task
-    top_1_used_champ = champ_info_result.get('top_1_used_champ', '')
+            results = await asyncio.gather(wiki_info_task, ingsingfull_info_task, mmr_task)
 
-    wiki_info_task = asyncio.create_task(wiki_info(top_1_used_champ))
-    ingsingfull_info_task = asyncio.create_task(ingsingfull_info(top_1_used_champ))
-    mmr_task = asyncio.create_task(mmr(name))
+            wiki_info_result = results[0]
+            ingsingfull_info_result = results[1]
+            mmr_result = results[2]
 
-    results = await asyncio.gather(wiki_info_task, ingsingfull_info_task, mmr_task)
-
-    wiki_info_result = results[0]
-    ingsingfull_info_result = results[1]
-    mmr_result = results[2]
-
-    return {
-        'champ_info': champ_info_result,
-        'wiki_info': wiki_info_result,
-        'ingsingfull_info': ingsingfull_info_result,
-        'mmr': mmr_result,
-    }
-       
+            return {
+                'champ_info': champ_info_result,
+                'wiki_info': wiki_info_result,
+                'ingsingfull_info': ingsingfull_info_result,
+                'mmr': mmr_result,
+            }
+    except Exception as e:
+        print(f'An error occurred in the main function: {e}')
+        return {}
 if __name__ == "__main__":
     print(asyncio.run(main('Twitch Oskr1938#1938')))
