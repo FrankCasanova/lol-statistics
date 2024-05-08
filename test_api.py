3cfd913e74e3881d039084987a1609b454a24852
unknown <frankcasanova.info@gmail.com>
2024-05-08 17:30:18 +0200

    some configurations to improve the performance

--- test_api.py
+++ test_api.py
@@ -1,45 +1,36 @@
-from curl_cffi import requests
+import asyncio
+from curl_cffi.requests import AsyncSession
+from asyncio import WindowsSelectorEventLoopPolicy
 
 
-url = "https://api.tracker.gg/api/v2/lol/standard/profile/riot/CHADUDYR%23UDYR?region=EUW"
+asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+
+async def main():
+    async with AsyncSession(impersonate='edge101') as client:
+        url = "https://api.tracker.gg/api/v2/lol/standard/profile/riot/CHADUDYR%23UDYR?region=EUW"
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Dnt": "1",
+            "Upgrade-Insecure-Requests": "1",
+            "Connection": "keep-alive",
+            "Cache-Control": "max-age=0",
+        }
+
+        payload = ""
+
+        response = await client.get(url, headers=headers, data=payload,)
+
+        print(response.text)
 
 
-# headers = {
-#     "cookie": "__cf_bm=aLfEOsXLW7ZBqAa5nP2.TW2R8O__j6bFSztgxtqo7MY-1715162182-1.0.1.1-rPjfYz74f8mgO3YtMAFce.UobGlySK5vjho_1RmGMN67LzPpuaBIRF0JaimpPZEpFSulzcmibRhJK91m1aEk6TmVlOYJGdl3cEAtsVFmlAg; cf_clearance=OPWcBgzFnqc4gTyJyOkgFaGHg_J2r_alPVDBq4u1FC8-1715162182-1.0.1.1-oncx1uuzDhK3HrGPaOuHWC8kIs2EIjLILG96vdfWjV_C7E89J6Yluq3ZlYyNLwE.r.FyunIRsGynd88tNbf55w; _ga=GA1.1.439411161.1715162175; session_id=a531145f-1969-4bc3-95f7-f9d8f8d18a7f; X-Mapping-Server=s12; __cflb=02DiuFQAkRrzD1P1mdm8JatZXtAyjoPD2CUUKCN3AcQSQ; _sharedid=cfa5acfb-b156-4367-b829-bb27ae70ef7e; _ga_HWSV72GK8X=GS1.1.1715162175.1.1.1715162199.0.0.0",
-#     "accept": "application/json, text/plain, */*",
-#     "accept-language": "en-US,en;q=0.9",
-#     "dnt": "1",
-#     "origin": "https://tracker.gg",
-#     "priority": "u=1, i",
-#     "referer": "https://tracker.gg/",
-#     "sec-ch-ua": "\"Chromium\";v=\"124\", \"Microsoft Edge\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-#     "sec-ch-ua-mobile": "?0",
-#     "sec-ch-ua-platform": "\"Windows\"",
-#     "sec-fetch-dest": "empty",
-#     "sec-fetch-mode": "cors",
-#     "sec-fetch-site": "same-site",
-#     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0"
-# }
+asyncio.run(main())
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-    "Dnt": "1",
-    "Upgrade-Insecure-Requests": "1",
-    "Connection": "keep-alive",
-    "Cache-Control": "max-age=0",
-}
-
-payload = ""
-
-
-response = requests.request("GET", url, data=payload,impersonate='edge101')
-
-# print(response.text)
-print(response.headers)
-print(response.status_code)
-print(response.cookies)
+# print(response.headers)
+# print(response.status_code)
+# print(response.cookies)
 
 
 
